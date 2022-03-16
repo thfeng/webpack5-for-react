39357a45eb8e8a027af8e49eee21d1c92b1e2ee3
thfeng <tianhaofeng726@sina.com>
2022-03-16 16:56:14 +0800

    update code

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './pages/Home';
 import { IntlProvider } from 'react-intl';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import PokemonList from './pages/pokemon/PokemonList';
 import Messages from '../locales/zh-CN.json';
-
 import './app.less';
 
 const App = () => {
   return (
     <IntlProvider messages={Messages} locale="zh-CN" defaultLocale="en-US">
       <h1>Webpack 5 with Typescript for React.js</h1>
-      <Home></Home>
+      <BrowserRouter>
+        <nav>
+          <Link to="/pokemon">View Pokemon</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/pokemon" element={<PokemonList></PokemonList>}></Route>
+        </Routes>
+      </BrowserRouter>
     </IntlProvider>
   );
 };
---
+++ src/pages/pokemon/PokemonList.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PokemonList: React.FC = () => <h2>Pokemon ALL</h2>;
+
+export default PokemonList;
